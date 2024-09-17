20f9b2d6c5e478338b2da3d4f8b091aab4f8aedc
JaneeshaJ2001 <wickramasinghejj.21@uom.lk>
2024-09-17 14:54:31 +0530

    manifesto comparator UI elements added

--- frontend/manifesto_comparator.py
+++ frontend/manifesto_comparator.py
@@ -3,42 +3,29 @@ import random
 import time
 
 def manifesto_comparator_page():
-    
-    # Streamed response emulator
-    def response_generator():
-        response = random.choice(
-            [
-                "Hello there! How can I assist you today?",
-                "Hi, human! Is there anything I can help you with?",
-                "Do you need help?",
-            ]
-        )
-        for word in response.split():
-            yield word + " "
-            time.sleep(0.05)
-
-
     st.title("Manifesto Comparator")
 
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    col1, col2 = st.columns(2)
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    with col1:
+        option1 = st.selectbox(
+            "Candidate 1 name",
+            ("Anura Kumara Dissanayake", "Sajith Premadasa", "Ranil Wickramasinghe", "Namal Rajapaksa"),
+            index=None,
+            placeholder="Select ...",
+        )
 
-    # Accept user input
-    if prompt := st.chat_input("What is up?"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    with col2:
+        option2 = st.selectbox(
+            "Candidate 2 name",
+            ("Anura Kumara Dissanayake", "Sajith Premadasa", "Ranil Wickramasinghe", "Namal Rajapaksa"),
+            index=None,
+            placeholder="Select ...",
+        )
+    
 
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            response = st.write_stream(response_generator())
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+
+    
+    
+    
+    
