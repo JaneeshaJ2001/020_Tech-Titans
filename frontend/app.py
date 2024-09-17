c1bbbf6d40d146297d997b918d68d2c54587f68d
JaneeshaJ2001 <wickramasinghejj.21@uom.lk>
2024-09-17 13:35:38 +0530

    main file added

---
+++ frontend/app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from sentiment_analysis import sentiment_analysis_page
+from electobot import electobot_page
+from manifesto_comparator import manifesto_comparator_page
+from home import home
+
+
+def main():
+    # Navigation bar
+    st.set_page_config(layout="wide")
+    
+
+    with st.sidebar:
+        selected = option_menu(
+        "SL President\'24",  # Title of the menu
+        ["Home", "Sentiment Analysis", "ElectoBot", "Manifesto Comparator"],  # Menu items
+        icons=["house", "camera", "clipboard", "book"],  # Icons for each item
+        menu_icon="app-indicator",  # Icon for the menu header
+        default_index=0,  # Default selected item
+        styles={
+            "container": {"padding": "5!important"},
+            "icon": {"color": "orange", "font-size": "25px"},  # Color and size for the icons
+            "nav-link": {"font-size": "18px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link-selected": {"background-color": "green", "color": "white"},
+        }
+    )    
+    
+    if selected == 'Home':  
+        home()
+
+        
+    elif selected == 'Sentiment Analysis':
+        sentiment_analysis_page()
+            
+        
+    elif selected == 'ElectoBot':
+        electobot_page()
+        
+    elif selected == 'Manifesto Comparator':
+        manifesto_comparator_page()
+
+if __name__ == '__main__':
+    main()
